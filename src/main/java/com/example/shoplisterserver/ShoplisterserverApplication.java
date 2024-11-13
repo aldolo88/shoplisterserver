0e8b96b309992fa85b4d955fde2bedc68c459383
aldolo88 <alberto.dorado88@gmail.com>
2024-11-13 23:46:04 +0100

    Initial commit

---
+++ src/main/java/com/example/shoplisterserver/ShoplisterserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoplisterserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoplisterserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoplisterserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shoplisterserver/ShoplisterserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoplisterserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoplisterserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
